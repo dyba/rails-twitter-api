2ef78fed7c3a76e79a1603beebed615585059cc1
Daniel Dyba <daniel.dyba@gmail.com>
2016-10-17 22:32:10 -0700

    Check in the test generator config

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,11 @@ module RailsTwitterApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.generators do |g|
+      g.orm :mongoid
+      g.test_framework :minitest, spec: false, fixture: false
+    end
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
