e886a265bf258d2ced02f2beea5ada47235f7181
Daniel Dyba <daniel.dyba@gmail.com>
2016-10-17 21:25:35 -0700

    Filters Base64 credentials

--- test/support/vcr.rb
+++ test/support/vcr.rb
@@ -3,4 +3,8 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'test/vcr_cassettes'
   config.hook_into :webmock
+  config.filter_sensitive_data('<BASE64_CREDENTIALS>') {
+    encoded_credentials = "#{URI.encode(ENV['TWITTER_CONSUMER_KEY'])}:#{URI.encode(ENV['TWITTER_CONSUMER_SECRET'])}"
+    Base64.strict_encode64(encoded_credentials)
+  }
 end
